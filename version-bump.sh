452b528e3297c073ca38e1a62efdbbf28db2d82d
razcoen <raz@icohens.com>
2022-07-12 17:38:14 +0300

    feat: create push & setup scripts for github actions

---
+++ version-bump.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BUMP_PATCH="patch"
+BUMP_MINOR="minor"
+BUMP_MAJOR="major"
+
+VERSION_FILE="VERSION"
+
+if [ ${#} -lt 1 ];
+then
+  echo "Usage: ${0} [ ${BUMP_PATCH} | ${BUMP_MINOR} | ${BUMP_MAJOR} ]"
+  exit 1
+fi
+
+if [ ${1} != ${BUMP_PATCH} ] && [ ${1} != ${BUMP_MINOR} ] && [ ${1} != ${BUMP_MAJOR} ];
+then
+  echo "Parameter [ ${1} ] should be any of [ ${BUMP_PATCH} | ${BUMP_MINOR} | ${BUMP_MAJOR} ]."
+  exit 1
+fi
+
+if [[ ! -z $(git status -s) ]]; then
+  echo "There are uncommitted changes, please make sure everything is committed."
+  exit 1
+fi
+
+
+version=$(cat ${VERSION_FILE})
+
+version_patch=$(echo $version | grep -Eo "[0-9]+$")
+version_minor=$(echo $version | grep -Eo "[0-9]+\.[0-9]+$" | grep -Eo "^[0-9]+")
+version_major=$(echo $version | grep -Eo "^[0-9]+")
+
+if [ ${1} = ${BUMP_PATCH} ];
+then
+  let "version_patch=version_patch+1"
+elif [ ${1} = ${BUMP_MINOR} ];
+then
+  version_patch=0
+  let "version_minor=version_minor+1"
+elif [ ${1} = ${BUMP_MAJOR} ];
+then
+  version_patch=0
+  version_minor=0
+  let "version_major=version_major+1"
+fi
+
+new_version="${version_major}.${version_minor}.${version_patch}"
+
+sed -i'.bak' -e "s/$version/$new_version/g" setup/action.yml
+sed -i'.bak' -e "s/$version/$new_version/g" ${VERSION_FILE}
+
+git add .
+git commit -m "chore: version bump ($version --> $new_version)"
+git tag "v$new_version"
