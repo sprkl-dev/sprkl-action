5681f31eedfe3a92866a0b06a1cff41d4705dd18
Eliran Maman <eliran@sprkl.dev>
2022-09-04 11:25:41 +0300

    chore: add sprkl ci push dashboard command

--- setup/src/post.ts
+++ setup/src/post.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as github from '@actions/github';
-
+import { existsSync, readFileSync } from 'fs';
 /**
  Push sprkl state to CI platfrom at the end of the job
  */
@@ -10,8 +10,35 @@ import * as github from '@actions/github';
     const token = core.getInput('token');
     const githubRepository = `${github.context.repo.owner}/${github.context.repo.repo}`
     const githubRunId = github.context.runId
+    const headSha = getHeadCommitSha();
 
     // push sprkl state to CI platfrom
-    const sprklPushCmd = `sprkl ci push --token=${token} --repository=${githubRepository} --run=${githubRunId}`
-    await exec.exec(sprklPushCmd);
+    const sprklPushDataCmd = `sprkl ci push snapshot --token=${token} --repository=${githubRepository} --run=${githubRunId}`
+    const sprklPushDashboardCmd = `sprkl ci push dashboard --token=${token} --repository=${githubRepository} --run=${githubRunId} --headSha=${headSha}`
+
+    await exec.exec(sprklPushDataCmd);
+    await exec.exec(sprklPushDashboardCmd);
 })();
+
+function getHeadCommitSha(): string {
+    const eventPath = process.env['GITHUB_EVENT_PATH']!;
+    if(!eventPath){
+        console.log('[Sprkl Action]: GITHUB_EVENT_PATH is missing, using GITHUB_SHA instead');
+        return github.context.sha;
+    }
+
+    if(!existsSync(eventPath)) {
+        console.log(`[Sprkl Action]: Event path ${eventPath} is not exists, using GITHUB_SHA instead`);
+        return github.context.sha;
+    }
+
+    try{
+        const eventData = readFileSync(eventPath, {flag: 'r', encoding: 'utf-8'});
+        const event = JSON.parse(eventData)
+        return event.pull_request.head.sha!;
+    }catch(e) {
+        console.log(`[Sprkl Action]: Could not read & parse github event from ${eventPath}, using GITHUB_SHA instead. ${e}`);
+        return github.context.sha;
+    }
+
+}
